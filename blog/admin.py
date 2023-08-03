ce8ad9d54e2b87ce4798604276152227212b2b9a
arian <ariankhademzadeh0@gamil.com>
2023-08-04 02:02:58 +0430

    added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import blog_Post
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'   
+    list_display = ('title' ,'counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    search_fields = ['title','content']
+
+admin.site.register(blog_Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from blog.models import blog_Post
+from django.utils import timezone
 # Create your views here.
 
 def blog_view(request):
-    return render(request, "blog/blog-home.html")
+    posts = blog_Post.objects.filter(status=1)
+    
+    return render(request, "blog/blog-home.html", {'posts': posts})
 
-def blog_single(request):
-    return render(request, "blog/blog-single.html")
+def blog_single(request, pid):
+    
+    post = get_object_or_404(blog_Post, id=pid)
+    
+    
+    
+    posts = blog_Post.objects.filter(published_date__lte=timezone.now())
+    
+    blog_object=blog_Post.objects.get(id=pid)
+    blog_object.blog_views += 1
+    blog_object.save()
+    
+        
+    return render(request, "blog/blog-single.html",{'test': post, 'tests': posts} )
