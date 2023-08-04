196436f494c5cf79bb713d575235fc82d09a5a1c
arian <ariankhademzadeh0@gamil.com>
2023-08-05 01:13:51 +0430

    added the database

--- blog/views.py
+++ blog/views.py
@@ -4,21 +4,16 @@ from django.utils import timezone
 # Create your views here.
 
 def blog_view(request):
-    posts = blog_Post.objects.filter(status=1)
-    
-    return render(request, "blog/blog-home.html", {'posts': posts})
+    posts = blog_Post.objects.filter(published_date__lte=timezone.now(), status=1)
+    context = {'posts': posts}
+    return render(request, "blog/blog-home.html",context)
 
 def blog_single(request, pid):
     
-    post = get_object_or_404(blog_Post, id=pid)
     
-    
-    
-    posts = blog_Post.objects.filter(published_date__lte=timezone.now())
-    
-    blog_object=blog_Post.objects.get(id=pid)
-    blog_object.blog_views += 1
+    blog_object = get_object_or_404(blog_Post, pk=pid)
+    blog_object.counted_views += 1
     blog_object.save()
     
-        
-    return render(request, "blog/blog-single.html",{'test': post, 'tests': posts} )
+    context = {'post':blog_object} 
+    return render(request, "blog/blog-single.html", context)
