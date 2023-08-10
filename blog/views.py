eee755adb3b05be6154b3f48d12fb77e164acfbd
arian <ariankhademzadeh0@gamil.com>
2023-08-10 21:12:17 +0430

    added the next and prev pages in single blog posts

--- blog/views.py
+++ blog/views.py
@@ -14,9 +14,9 @@ def blog_single(request, pid):
     post.counted_views += 1
     post.save()
   
-    next_post = blog_Post.objects.filter(id__gt=post.id).order_by('id').first()
-    prev_post = blog_Post.objects.filter(id__lt=post.id).order_by('id').last()
+    nextpost = blog_Post.objects.filter(id__gt=post.id).order_by('id').first()
+    prevpost = blog_Post.objects.filter(id__lt=post.id).order_by('-id').first()
     
     
-    context = {'post':post, 'next_post': next_post, 'prev_post': prev_post} 
+    context = {'post':post, 'nextpost': nextpost, 'prevpost': prevpost} 
     return render(request, "blog/blog-single.html", context)
