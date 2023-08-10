06114416fd35c06f2ec35a345f3d357cc96ca911
arian <ariankhademzadeh0@gamil.com>
2023-08-10 14:17:19 +0430

    added the next and prev pages to views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import blog_Post
 from django.utils import timezone
+
 # Create your views here.
 
 def blog_view(request):
@@ -9,11 +10,13 @@ def blog_view(request):
     return render(request, "blog/blog-home.html",context)
 
 def blog_single(request, pid):
-    
-    
     post = blog_Post.objects.get(published_date__lte=timezone.now(), status=1, pk=pid)
     post.counted_views += 1
     post.save()
+  
+    next_post = blog_Post.objects.filter(id__gt=post.id).order_by('id').first()
+    prev_post = blog_Post.objects.filter(id__lt=post.id).order_by('id').last()
     
-    context = {'post':post} 
+    
+    context = {'post':post, 'next_post': next_post, 'prev_post': prev_post} 
     return render(request, "blog/blog-single.html", context)
