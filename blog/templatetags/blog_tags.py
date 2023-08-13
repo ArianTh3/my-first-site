1d2b69ae4f7a0722d90d8a6025c905a1ae364f7f
arian <ariankhademzadeh0@gamil.com>
2023-08-13 13:55:07 +0430

    added

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,10 @@
 from django import template
+from blog.models import blog_Post
+
 
 register = template.Library() 
+
+@register.inclusion_tag("blog/blog-popular-post.html")
+def latestpost(args=3):
+    posts = blog_Post.objects.filter(status=1).order_by('published_date')[:args]
+    return {"posts":posts}
