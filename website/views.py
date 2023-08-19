ea5213c9708819b31205204dd4bbf733a313a510
arian <ariankhademzadeh0@gamil.com>
2023-08-19 13:39:47 +0430

    added the contact part

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from website.forms import ContactForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request, "website/index.html")
@@ -8,6 +10,14 @@ def about_view(request):
     return render(request, "website/about.html")
 
 def contact_view(request):
-    return render(request, "website/contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "your ticket submited")
+        else:
+            messages.add_message(request, messages.ERROR, "your ticket did not submited")
+    form = ContactForm()
+    return render(request, "website/contact.html", {"form":form})
 
 
