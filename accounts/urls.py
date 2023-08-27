8a574aacc502968e0e767bf9fd125b77eacaaed2
arian <ariankhademzadeh0@gamil.com>
2023-08-27 21:27:02 +0430

    added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login', login_view, name='login'),
-    #path('logout', logout_view, name='logout'),
+    path('logout', logout_view, name='logout'),
     path('register', register_view, name='register'),
     
 ]
