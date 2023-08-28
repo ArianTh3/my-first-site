dc688d36fc2aa7599949b2d69dbacea444a0f594
arian <ariankhademzadeh0@gamil.com>
2023-08-28 14:16:25 +0430

    added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -27,4 +27,14 @@ def logout_view(request):
     return redirect('/')
     
 def register_view(request):
-    return render(request, 'accounts/register.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        form = UserCreationForm()
+        return render(request, 'accounts/register.html', {"form":form})
+    else:
+        return redirect('/')
+    
